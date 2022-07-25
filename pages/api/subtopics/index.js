c7aeb0e1c5d7ccd6cd467ee62569babc80f606e0
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2022-07-25 14:55:00 +0200

    added endpoints for subtopics and categories

--- pages/api/subtopics/index.js
+++ pages/api/subtopics/index.js
@@ -1,5 +1,21 @@
 import { subtopics } from '../../../data/subtopics'
+import initMiddleware from '../../../lib/init-middleware'
+import Cors from 'cors'
 
-export default function handler(req, res) {
+// Initialize the cors middleware
+const cors = initMiddleware(
+    // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+    Cors({
+        // Only allow requests with GET, POST and OPTIONS
+        methods: ['GET', 'POST', 'OPTIONS'],
+    })
+)
+
+export default async function handler(req, res) {
+    // Run cors
+    await cors(req, res)
+
+    // Rest of the API logic
+    
     res.status(200).json(subtopics)
 }
