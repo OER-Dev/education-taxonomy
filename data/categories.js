5716b8baccac4bb0cf7b039ef2383e8630a7562e
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2022-07-26 13:27:43 +0200

    Changed field names in api

--- data/categories.js
+++ data/categories.js
@@ -1,26 +1,26 @@
 export const categories = [
     {
         img: "https://digitallibrary.io/wp-content/uploads/2022/02/Counting.png",
-        title: "Counting"
+        topic: "Counting"
     },
     {
         img: "https://digitallibrary.io/wp-content/uploads/2022/02/Adding-1s-or-10s-1.png",
-        title: "Addition within 100"
+        topic: "Addition within 100"
     },
     {
         img: "https://digitallibrary.io/wp-content/uploads/2022/02/Measuring-length.png",
-        title: "Length"
+        topic: "Length"
     },
     {
         img: "https://digitallibrary.io/wp-content/uploads/2022/03/Money-theme-illlustration.png",
-        title: "Money"
+        topic: "Money"
     },
     {
         img: "https://digitallibrary.io/wp-content/uploads/2022/03/Money-theme-illlustration.png",
-        title: "Money"
+        topic: "Money"
     },
     {
         img: "https://digitallibrary.io/wp-content/uploads/2022/02/Adding-1s-or-10s-1.png",
-        title: "Addition within 100"
+        topic: "Addition within 100"
     }
     ]
--- data/subtopics.js
+++ data/subtopics.js
@@ -1,22 +1,22 @@
 export const subtopics = [
     {
     img: "https://digitallibrary.io/wp-content/uploads/2022/07/Skjermbilde-2022-07-20-kl.-10.01.56.png",
-    location: "Life skills",
-    distance: "Critical thinking - creativety"
+    topic: "Life skills",
+    description: "Critical thinking - creativety"
     },
     {
     img: "https://digitallibrary.io/wp-content/uploads/2022/07/Skjermbilde-2022-07-20-kl.-10.42.01.png",
-    location: "Our planet",
-    distance: "Sustainable development"
+    topic: "Our planet",
+    description: "Sustainable development"
     },
     {
     img: "https://digitallibrary.io/wp-content/uploads/2022/07/Skjermbilde-2022-07-20-kl.-09.52.49.png",
-    location: "Digital skills",
-    distance: "Web, mobile and more"
+    topic: "Digital skills",
+    description: "Web, mobile and more"
     },
     {
     img: "https://digitallibrary.io/wp-content/uploads/2022/07/Skjermbilde-2022-07-20-kl.-09.50.18.png",
-    location: "Science",
-    distance: "4-hour drive"
+    topic: "Science",
+    description: "4-hour drive"
     }
     ]
