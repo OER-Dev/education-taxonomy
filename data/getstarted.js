a721a82bdd75b1a3707ca6d1cf5cc78b63cde721
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2022-07-27 14:43:12 +0200

    Added get started to api

---
+++ data/getstarted.js
@@ -0,0 +1,9 @@
+export const getstarted = [
+    {
+        img: "https://digitallibrary.io/wp-content/uploads/2022/07/Math.png",
+        topic: "Find you math level"
+    },
+    {
+        img: "https://digitallibrary.io/wp-content/uploads/2022/07/Reading.png",
+        topic: "Find your reading level"
+    }]
