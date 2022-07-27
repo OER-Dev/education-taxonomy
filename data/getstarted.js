22a65bae219dafa4c59f6f43445b5daf74c3a8e1
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2022-07-27 16:49:56 +0200

    fix back

--- data/getstarted.js
+++ data/getstarted.js
@@ -1,6 +1,6 @@
 export const getstarted = [
     {
-        img: "https://digitallibrary.io/wp-content/uploads/2022/07/Reading.png",
+        img: "https://digitallibrary.io/wp-content/uploads/2022/07/Math.png",
         topic: "Find you math level"
     },
     {
