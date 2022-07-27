5bda26ba90f9753a3747e2f8d03e274a225fa2e4
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2022-07-27 17:01:14 +0200

    Topy in text

--- data/getstarted.js
+++ data/getstarted.js
@@ -1,7 +1,7 @@
 export const getstarted = [
     {
         img: "https://digitallibrary.io/wp-content/uploads/2022/07/Math.png",
-        topic: "Find you math level"
+        topic: "Find your math level"
     },
     {
         img: "https://digitallibrary.io/wp-content/uploads/2022/07/Reading.png",
