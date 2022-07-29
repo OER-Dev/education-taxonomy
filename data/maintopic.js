0e450d6886b62faca3c4e1a0817e744ac74b5655
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2022-07-29 09:59:42 +0200

    Added URL in APIs endpoints

--- data/maintopic.js
+++ data/maintopic.js
@@ -2,11 +2,13 @@ export const maintopic = [
     {
     img: "https://digitallibrary.io/wp-content/uploads/2022/07/Skjermbilde-2022-07-20-kl.-10.37.42.png",
     topic: "Math",
-    description: "Fun videos and games"
+    description: "Fun videos and games",
+    url: "https://digitallibrary.io/topic/early-math/"
     },
     {
     img: "https://digitallibrary.io/wp-content/uploads/2022/07/Skjermbilde-2022-07-20-kl.-10.35.48.png",
     topic: "Reading",
-    description: "Books and games"
+    description: "Books and games",
+    url: "https://digitallibrary.io/topic/library-books/"
     }
     ]
--- data/subtopics.js
+++ data/subtopics.js
@@ -2,21 +2,25 @@ export const subtopics = [
     {
     img: "https://digitallibrary.io/wp-content/uploads/2022/07/Skjermbilde-2022-07-20-kl.-10.01.56.png",
     topic: "Life skills",
-    description: "Critical thinking - creativety"
+    description: "Critical thinking - creativety",
+    url: "https://digitallibrary.io/topic/collections/our-planet/"
     },
     {
     img: "https://digitallibrary.io/wp-content/uploads/2022/07/Skjermbilde-2022-07-20-kl.-10.42.01.png",
     topic: "Our planet",
-    description: "Sustainable development"
+    description: "Sustainable development",
+    url: "https://digitallibrary.io/topic/collections/our-planet/"
     },
     {
     img: "https://digitallibrary.io/wp-content/uploads/2022/07/Skjermbilde-2022-07-20-kl.-09.52.49.png",
     topic: "Digital skills",
-    description: "Web, mobile and more"
+    description: "Web, mobile and more",
+    url: "https://digitallibrary.io/topic/collections/our-planet/"
     },
     {
     img: "https://digitallibrary.io/wp-content/uploads/2022/07/Skjermbilde-2022-07-20-kl.-09.50.18.png",
     topic: "Science",
-    description: "4-hour drive"
+    description: "4-hour drive",
+    url: "https://digitallibrary.io/topic/collections/science/"
     }
     ]
