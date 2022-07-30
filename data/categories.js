69c4ea16efcf2186441b0ffd7fb796254cca8a12
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2022-07-30 13:15:10 +0200

    Added more data to endpoints

--- data/categories.js
+++ data/categories.js
@@ -1,26 +1,32 @@
 export const categories = [
     {
         img: "https://digitallibrary.io/wp-content/uploads/2022/02/Counting.png",
-        topic: "Counting"
+        topic: "Counting",
+        url: "https://digitallibrary.io/topic/early-math/counting/"
     },
     {
         img: "https://digitallibrary.io/wp-content/uploads/2022/02/Adding-1s-or-10s-1.png",
-        topic: "Addition within 100"
+        topic: "Addition within 100",
+        url: "https://digitallibrary.io/topic/early-math/addition-within-100/"
     },
     {
         img: "https://digitallibrary.io/wp-content/uploads/2022/02/Measuring-length.png",
-        topic: "Length"
+        topic: "Length",
+        url: "https://digitallibrary.io/topic/early-math/length-size/"
     },
     {
         img: "https://digitallibrary.io/wp-content/uploads/2022/03/Money-theme-illlustration.png",
-        topic: "Money"
+        topic: "Money",
+        url: "https://digitallibrary.io/topic/early-math/money/"
     },
     {
-        img: "https://digitallibrary.io/wp-content/uploads/2022/03/Money-theme-illlustration.png",
-        topic: "Money"
+        img: "https://digitallibrary.io/wp-content/uploads/2022/02/Numbers-0-%E2%80%93-120.png",
+        topic: "Numbers 0 - 120",
+        url: "https://digitallibrary.io/topic/early-math/numbers-0-120/"
     },
     {
-        img: "https://digitallibrary.io/wp-content/uploads/2022/02/Adding-1s-or-10s-1.png",
-        topic: "Addition within 100"
+        img: "https://digitallibrary.io/wp-content/uploads/2022/02/Word-problems-within-100.png",
+        topic: "Word problems within 100",
+        url: "https://digitallibrary.io/topic/early-math/word-problems-within-100/"
     }
     ]
